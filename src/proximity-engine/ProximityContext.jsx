690e71f7d37baa767c732b3eb6f9da4b33b8c793
Yousef Zedan <zedstudios.devs@gmail.com>
2026-04-21 15:25:46 +0200

    GEORAMA

--- src/proximity-engine/ProximityContext.jsx
+++ src/proximity-engine/ProximityContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 
 const ProximityContext = createContext({
-  defaultFont: "'Bricolage Grotesque', sans-serif",
+  defaultFont: "'Georama', sans-serif",
 });
 
 export const ProximityProvider = ({ children, config }) => {
